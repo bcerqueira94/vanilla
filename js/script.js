f339380bad3c7e0c20e2b3c4fc52fe7cbe273a1e
Bruno Cerqueira <bcerqueira94@gmail.com>
2023-03-30 20:48:22 -0300

    Loader

--- js/script.js
+++ js/script.js
@@ -3,38 +3,69 @@ $("#year").text(new Date().getFullYear());
 
 //Inicializando AOS
 AOS.init({
-    duration: 700,
-    easing: 'ease-out'
+  duration: 700,
+  easing: 'ease-out'
 });
 
-$(document).ready(function() {
-    // Define a lista de imagens
-    var images = $(".background .background-img");
-    var index = 0;
-  
-    // Define o tempo de transição e o intervalo de tempo para trocar as imagens
-    var time = 10000; // 5 segundos
-  
-    // Ativa a primeira imagem
-    $(images[index]).addClass("active");
-  
-    // Define a função para trocar a imagem
-    function changeBackground() {
-      // Fade out da imagem atual
-      $(images[index]).removeClass("active");
-      index++;
-      if (index == images.length) {
-        index = 0;
-      }
-      // Fade in da nova imagem
-      $(images[index]).addClass("active");
+$(document).ready(function () {
+
+  $('#main-content').hide();
+
+  // Define a lista de imagens
+  var images = $(".background .background-img");
+  var index = 0;
+
+  // Define o tempo de transição e o intervalo de tempo para trocar as imagens
+  var time = 10000; // 5 segundos
+
+  // Ativa a primeira imagem
+  $(images[index]).addClass("active");
+
+  // Define a função para trocar a imagem
+  function changeBackground() {
+    // Fade out da imagem atual
+    $(images[index]).removeClass("active");
+    index++;
+    if (index == images.length) {
+      index = 0;
     }
-  
-    // Chama a função para trocar a imagem a cada intervalo de tempo
-    setInterval(changeBackground, time);
-  });
+    // Fade in da nova imagem
+    $(images[index]).addClass("active");
+  }
+
+  // Chama a função para trocar a imagem a cada intervalo de tempo
+  setInterval(changeBackground, time);
+});
 
 
 // CSS selector
 new Ukiyo(".ukiyo")
 
+// Animação
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('vnle-loader'),
+  renderer: 'svg',
+  loop: false,
+  autoplay: false,
+  path: 'vnle.json'
+})
+
+//Delay Entrada Animação
+setTimeout(function () {
+  animation.play();
+}, 600);
+
+//Delay Encerramento Animação
+setTimeout(function () {
+  $('#vnle-loader').fadeOut(1000);
+}, 3500);
+
+//Retirar-Pre-loader 
+setTimeout(function() {
+  $("#pre-loader").hide();
+}, 3900);
+
+//Exibir main-content
+setTimeout(function() {
+  $('#main-content').fadeIn(500);
+}, 4500);
