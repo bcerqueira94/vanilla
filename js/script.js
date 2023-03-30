7a6608a03b16228639fec0a2879cbce4c2a6f8f2
Bruno Cerqueira <bcerqueira94@gmail.com>
2023-03-29 23:00:41 -0300

    Commit

--- js/script.js
+++ js/script.js
@@ -6,3 +6,35 @@ AOS.init({
     duration: 700,
     easing: 'ease-out'
 });
+
+$(document).ready(function() {
+    // Define a lista de imagens
+    var images = $(".background .background-img");
+    var index = 0;
+  
+    // Define o tempo de transição e o intervalo de tempo para trocar as imagens
+    var time = 5000; // 5 segundos
+  
+    // Ativa a primeira imagem
+    $(images[index]).addClass("active");
+  
+    // Define a função para trocar a imagem
+    function changeBackground() {
+      // Fade out da imagem atual
+      $(images[index]).removeClass("active");
+      index++;
+      if (index == images.length) {
+        index = 0;
+      }
+      // Fade in da nova imagem
+      $(images[index]).addClass("active");
+    }
+  
+    // Chama a função para trocar a imagem a cada intervalo de tempo
+    setInterval(changeBackground, time);
+  });
+
+
+// CSS selector
+new Ukiyo(".ukiyo")
+
